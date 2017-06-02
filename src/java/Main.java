0ecc975c0baf04c2e058193131a4336b71e6b59d
1396912801@qq.com <wangchong.960818>
2017-06-02 17:12:16 +0800

    [DB作业]User相关操作完成，添加Account 和 AccountMapper ing...

--- src/java/Main.java
+++ src/java/Main.java
@@ -6,15 +6,6 @@ import java.util.Scanner;
  */
 public class Main {
 	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		int i = 1;
-		while (in.hasNext()){
-			if(i==0){
-				System.out.println();
-			}else i=0;
-			BigInteger a = new BigInteger(in.next());
-			BigInteger b = a.add(new BigInteger("1")).multiply(a).divide(new BigInteger("2"));
-			System.out.print(b);
-		}
+
 	}
 }
